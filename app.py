b0487f047a0a30f9bba903745aad60b9bed6a602
Sabina Matejčíková <matejcikovaas@gmail.com>
2026-01-13 16:46:56 +0100

    cli version, parser

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
 from core.model import Project, TimeEntry
 from core.tracker import TimeTracker
 
+import argparse
 import time
 
 tracker = TimeTracker()
-project = Project("pks")
+active_project = None
 
-tracker.start(project)
-time.sleep(2)
-tracker.stop()
+def main():
+    parser = argparse.ArgumentParser(description = "Time Tracker CLI")
 
-print(tracker.total_time_seconds(project))
+    subparsers = parser.add_subparsers(dest="command")
+
+    start_parser = subparsers.add_parser("start")
+    start_parser.add_argument("project")
+
+    end_parser = subparsers.add_parser("stop")
+
+    args = parser.parse_args()
+
+    global active_project
+    
+    if args.command == "start":
+        active_project = Project(args.project)
+        tracker.start(active_project)
+        print(f"Started tracking project '{args.project}'")
+    
+    elif args.command == "stop":
+        tracker.stop()
+        seconds = tracker.total_time_seconds(active_project)
+
+        print(f"Project: {active_project.name}")
+        print(f"Time spend: {seconds}")
+
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
+
