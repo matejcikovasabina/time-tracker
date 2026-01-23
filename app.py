2a1dc33d46669cdd61c68f0b8580b9c4a0c54bd4
Sabina Matejčíková <matejcikovaas@gmail.com>
2026-01-23 16:41:23 +0100

    cli and gui funcionality

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-# main.py
 import sys
-
 from ui.ui import gui_main
+from ui.cli import cli_main
 
 def main():
-    gui_main()   
-
+    if len(sys.argv) > 1:
+        cli_main()
+    else:
+        gui_main()
 
 if __name__ == "__main__":
     main()
